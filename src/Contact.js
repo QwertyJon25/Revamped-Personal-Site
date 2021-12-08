b8783af493b5dd63b9eb85a9cfc8a62e49d43028
Jon Courtier <jonaqwerty@yahoo.com>
2021-12-08 00:31:00 -0500

    positioning

--- src/Contact.js
+++ src/Contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
                 <h3>We can shoot the breeze and spin some yarns.</h3>
                 <p className="contact-message">I'm always eager to make connections and avail the resources I have available
                     at my disposal. Help me help you!</p>
-                <a className="btn" href="mailto:jonaqwerty@yahoo.com">CONTACT ME</a>
+                <a className="btn" href="mailto:courtier.jon@gmail.com">CONTACT ME</a>
             </div>
     )
 }
