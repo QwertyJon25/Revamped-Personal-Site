7a036615737839a205bbc506254193493c9aeb18
Jon Courtier <jonaqwerty@yahoo.com>
2022-06-14 15:18:08 -0400

    heroku deployment merge

--- src/Bottom.js
+++ src/Bottom.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Bottom() {
     return (
         <div className="bottom-container">
-            <a className="footer-link" href="https://www.instagram.com/qwertyjon25/">Instagram</a>
+            <a className="footer-link" href="https://www.instagram.com/qwertyjon25/">Instagram</a><br/>
             <a className="footer-link" href="https://www.linkedin.com/in/jon-courtier/">LinkedIn</a>
             <p className="copyright">© 2022 Jon Courtier.</p>
         </div>
